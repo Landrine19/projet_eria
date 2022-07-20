36b8bf222db8fd1e1014185c8fe95bcffe32e8ae
Landrine19 <landrine.boatrin@gmail.com>
2022-07-20 14:03:34 +0000

    [add] format message

--- app/Models/Typeevenement.php
+++ app/Models/Typeevenement.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -13,5 +14,10 @@ class Typeevenement extends Model
     public function evenements()
     {
     	return $this->hasMany(Evenement::class);
+    }
+
+    public function getCreatedAtAttribute($vale)
+    {
+        return Carbon::parse($vale)->locale('fr')->isoFormat('lll');
     }
 }
